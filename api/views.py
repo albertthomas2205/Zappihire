7f30168b03d933fd2f914f56e35f7b74b1ee072d
albertthomas2205 <122583969+albertthomas2205@users.noreply.github.com>
2024-06-13 13:29:01 +0530

    finished

--- api/views.py
+++ api/views.py
@@ -15,10 +15,10 @@ class FetchWeatherDataView(APIView):
         
         # Generate random latitude and longitude
         lat = round(random.uniform(8.0667, 37.1), 6)  # Latitude: 8° 4′ to 37° 6′ north
-        lon = round(random.uniform(68.1167, 97.4167), 6)  # Round to 6 decimal places
+        lon = round(random.uniform(68.1167, 97.4167), 6)  # Longitude: 68° 7′ to 97° 25′ east
         # lat = -39.667775
         # lon =  11.401196
-        print(lat,lon)
+        print(lat, lon)
         url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}'
         
         response = requests.get(url)
@@ -28,6 +28,14 @@ class FetchWeatherDataView(APIView):
         location = data.get('name', 'Unknown Location')  # Use 'Unknown Location' if name is missing
         if location == 'Unknown Location':
             return Response({"error": "Location not found in API response."}, status=400)
+
+        # Check if the location already exists in the database
+        try:
+            existing_weather_data = WeatherData.objects.get(location=location)
+            serializer = WeatherDataSerializer(existing_weather_data)
+            return Response(serializer.data)
+        except WeatherData.DoesNotExist:
+            pass  # Continue to save the new weather data
 
         timestamp = timezone.now()
         temperature = data['main']['temp'] - 273.15  # Convert from Kelvin to Celsius
@@ -43,11 +51,8 @@ class FetchWeatherDataView(APIView):
         }
 
         # Write data to file in JSON format
-      
-        if weather_data['location']:
-                
-            with open('weather_data.txt', 'a') as file:
-                file.write(json.dumps(weather_data) + '\n')
+        with open('weather_data.txt', 'a') as file:
+            file.write(json.dumps(weather_data) + '\n')
 
         # Save to database
         serializer = WeatherDataSerializer(data=weather_data)
